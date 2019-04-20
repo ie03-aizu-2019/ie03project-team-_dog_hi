06722956f251e11a1a10e90be1ce949322b353d6
keronori <it0ka4ge8i@gmail.com>
2019-04-20 12:29:54 +0900

    create solution code Problem1 and Problem2

--- phase1_1.java
+++ phase1_1.java
@@ -8,48 +8,53 @@ public class phase1_1 {
 
     //main method
     public static void main(String[] argv){
+	Point p1 = new Point();
         input();
-        search_intersection();
+        p1=search_intersection(0, 1);
+	output(p1);
     }
 
-    public static void search_intersection(){
+    public static Point search_intersection(int i, int j){
         double matrix_a;    // matrix_a is |A|
         boolean exist_intersection = false;
-        matrix_a = (x[e_q[0]] - x[b_p[0]]) * (y[b_p[1]] - y[e_q[1]]) + (x[e_q[1]] - x[b_p[1]]) * (y[e_q[0]] - y[b_p[0]]);
+	Point point1 = new Point();
+        matrix_a = (x[e_q[i]] - x[b_p[i]]) * (y[b_p[j]] - y[e_q[j]]) + (x[e_q[j]] - x[b_p[j]]) * (y[e_q[i]] - y[b_p[i]]);
         //step1
         if(matrix_a <= EPS  && -EPS <= matrix_a){
             exist_intersection = false;
         } else {
             exist_intersection = true;
             //step2 (calculate s and t)
-            s = ((y[b_p[1]] - y[e_q[1]]) * (x[b_p[1]] - x[b_p[0]]) + (x[e_q[1]] - x[b_p[1]]) * (y[b_p[1]] - y[b_p[0]])) / matrix_a;
-            t = ((y[b_p[0]] - y[e_q[0]]) * (x[b_p[1]] - x[b_p[0]]) + (x[e_q[0]] - x[b_p[0]]) * (y[b_p[1]] - y[b_p[0]])) / matrix_a;
+            s = ((y[b_p[j]] - y[e_q[j]]) * (x[b_p[j]] - x[b_p[i]]) + (x[e_q[j]] - x[b_p[j]]) * (y[b_p[j]] - y[b_p[i]])) / matrix_a;
+            t = ((y[b_p[i]] - y[e_q[i]]) * (x[b_p[j]] - x[b_p[j]]) + (x[e_q[i]] - x[b_p[i]]) * (y[b_p[j]] - y[b_p[i]])) / matrix_a;
         }
         if(exist_intersection == true){
             //step3
             if(0<= s && s<=1  && 0<=t && t<=1){
                 exist_intersection = true;
                 //step4
-                x_intersection = x[b_p[0]] + (x[e_q[0]] - x[b_p[0]]) * s;   //x = x_P1 + (x_Q1 - x_P1)*s
-                y_intersection = y[b_p[0]] + (y[e_q[0]] - y[b_p[0]]) * s;
+                x_intersection = x[b_p[i]] + (x[e_q[i]] - x[b_p[i]]) * s;   //x = x_P1 + (x_Q1 - x_P1)*s
+                y_intersection = y[b_p[i]] + (y[e_q[i]] - y[b_p[i]]) * s;
             } else {
                 exist_intersection = false;
             }
         }
 
         //search for contact point
-        for(int i=0; i<N ;i++){
-            if(x[i]==x_intersection && y[i]==y_intersection){
+        for(int k=0; k<N ;k++){
+            if(x[k]==x_intersection && y[k]==y_intersection){
                 exist_intersection = false;
             }
         }
         //print
         if(exist_intersection == true){
-            System.out.println(String.format("%.5f",x_intersection));
-            System.out.println(String.format("%.5f",y_intersection));
+	    point1.x = x_intersection;
+	    point1.y = y_intersection;
         } else {
-            System.out.println("NA");
+	    point1.x = 0;
+	    point1.y = 0;
         }
+	return point1;
     }
 
     public static void input(){
@@ -71,4 +76,14 @@ public class phase1_1 {
             e_q[i] = scan.nextInt() - 1;
         }
     }
+
+    public static void output(Point point){
+	System.out.print(String.format("%.5f", point.x));
+	System.out.println(String.format(" %.5f", point.y));
+    }
+}
+
+class Point {
+    double x;
+    double y;
 }
